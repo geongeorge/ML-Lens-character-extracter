efbc549a82558d245b3875f7a03088f688b46fa7
Geon George <geongeorge98@gmail.com>
2020-07-26 15:30:12 +0530

    Adds vue persist

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['@/plugins/antd-ui'],
+  plugins: ['@/plugins/antd-ui', { src: '~/plugins/vuex-persist', ssr: false }],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
